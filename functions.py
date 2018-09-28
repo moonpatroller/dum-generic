ffa49c3b1eb2cccaaf20e71ed59355b613bff24b
moonpatroller <mrjoshbgoldstein@gmail.com>
2018-09-28 15:54:52 -0700

    removing getFreeKey

--- functions.py
+++ functions.py
@@ -1,17 +1,7 @@
 import time
 import os
 
-# Function for returning a first available key value for appending a new element to a dictionary
-def getFreeKey(itemsDict):
-    try:
-        for x in range(0, len(itemsDict) + 1):
-            if len(itemsDict[x]) > 0:
-                pass
-    except Exception:
-        pass
-    return(x)
-
-# Function used for loggin messages to stdout and a disk file
+# Function used for logging messages to stdout and a disk file
 def log(content, type):
     logfile = 'dum.log'
     print(str(time.strftime("%d/%m/%Y") + " " + time.strftime("%I:%M:%S") + " [" + type + "] " + content))
